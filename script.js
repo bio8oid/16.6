2c2e28425f8db5991796ac856e808624b5b551bb
bio8oid <8bollod8@mail.co.uk>
2019-04-20 20:11:28 +0200

    Add task script and flag feature

---
+++ script.js
@@ -0,0 +1,49 @@
+'use strict';
+(function(){ 
+
+
+var url = 'https://restcountries.eu/rest/v2/name/';
+var countriesList = document.getElementById('countries');
+
+function searchCountries() {
+    var countryName = document.getElementById('country-name').value;
+    if(!countryName.length) countryName = 'Poland';
+    fetch(url + countryName)
+        .then(function(resp) {
+            return resp.json();
+        })
+        .then(showCountriesList);
+}
+
+
+function showCountriesList(resp) {
+    countriesList.innerHTML = '';
+    resp.forEach(function(item){
+    var liEl = document.createElement('li');
+    var img = document.createElement('img');
+    liEl.innerText = item.name + ' (capital: ' + item.capital + ' )' ;
+    countriesList.appendChild(liEl);
+    countriesList.appendChild(img);
+    flag();
+
+
+    function flag() {
+      var flag = document.getElementsByTagName("img")[0];
+      var src = document.createAttribute("src");
+      src.value = item.flag;
+      flag.setAttributeNode(src);
+    }
+    });
+}
+
+
+
+
+
+document.getElementById('search').addEventListener('click', searchCountries);
+
+
+})(); 
+
+
+// + '<img src="' + item.flag + '" alt="' + countryName + '">'
