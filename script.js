76b99b2876bc7f1a6edfa9c1eb712e04ef139b49
bio8oid <8bollod8@mail.co.uk>
2019-10-12 18:52:37 +0200

    Solve each item image display issue and submit on enter

--- script.js
+++ script.js
@@ -2,48 +2,34 @@
 (function(){ 
 
 
-var url = 'https://restcountries.eu/rest/v2/name/';
-var countriesList = document.getElementById('countries');
+const url = 'https://restcountries.eu/rest/v2/name/';
+const countriesList = document.getElementById('countries');
 
-function searchCountries() {
-    var countryName = document.getElementById('country-name').value;
+function searchCountries(event) {
+    event.preventDefault();
+    let countryName = document.getElementById('country-name').value;
     if(!countryName.length) countryName = 'Poland';
-    fetch(url + countryName + '?fullText=true')
+    fetch(url + countryName)
         .then(function(resp) {
             return resp.json();
         })
         .then(showCountriesList);
 }
 
-
 function showCountriesList(resp) {
     countriesList.innerHTML = '';
     resp.forEach(function(item){
-    var liEl = document.createElement('li');
-    var img = document.createElement('img');
-    liEl.innerText = item.name + ' (capital: ' + item.capital + ' )' ;
-    countriesList.appendChild(liEl);
-    countriesList.appendChild(img);
-    flag();
-
-
-    function flag() {
-      var flag = document.getElementsByTagName("img")[0];
-      var src = document.createAttribute("src");
-      src.value = item.flag;
-      flag.setAttributeNode(src);
-    }
+    const listElement = document.createElement('li');
+    const img = document.createElement('img');
+    listElement.innerText = item.name + " (capital: " + item.capital + " )" ;
+    countriesList.appendChild(listElement);
+    const src = document.createAttribute("src");
+    src.value = item.flag;
+    listElement.appendChild(img).setAttributeNode(src);
     });
 }
 
-
-
-
+// document.getElementById('search').addEventListener('click', searchCountries);
 
 document.getElementById('search').addEventListener('click', searchCountries);
-
-
 })(); 
-
-
-// + '<img src="' + item.flag + '" alt="' + countryName + '">'
